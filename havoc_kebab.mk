be8737f15bb85f732b7e2b6ed1467daead17893c
cjh1249131356 <cjh1249131356@gmail.com>
2021-01-28 15:29:18 +0800

    kebab: Havoc bringup

--- havoc_kebab.mk
+++ havoc_kebab.mk
@@ -21,10 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kebab
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps Config
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := havoc_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
