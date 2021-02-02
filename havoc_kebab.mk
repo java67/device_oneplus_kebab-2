bd66341889bee3c95ef38399406fabb69c782bc5
cjh1249131356 <cjh1249131356@gmail.com>
2021-02-02 16:59:40 +0800

    kebab: Update to Redfin's Feb 2021 fingerprint

--- havoc_kebab.mk
+++ havoc_kebab.mk
@@ -44,4 +44,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus8T \
     PRIVATE_BUILD_DESC="OnePlus8T-user 11 RP1A.201005.001 2011101425 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
